333c35c8c3ae06a30d951b4913d3627878ce9acd
Jörg W Mittag <JoergWMittag+Almondbread@GoogleMail.Com>
2010-08-14 16:55:52 +0200

    Reduce the forest of parentheses for readability.

--- haskell/almondbread.hs
+++ haskell/almondbread.hs
@@ -3,7 +3,7 @@ import Data.Complex
 width = 80
 height = 20
 
-escapeTime c = length (take 255 (escapeSeries c))
+escapeTime c = length $ take 255 $ escapeSeries c
   where
     escapeSeries c = takeWhile notEscaping mandelSeries
       where
@@ -11,6 +11,6 @@ escapeTime c = length (take 255 (escapeSeries c))
         mandelSeries = iterate (\z -> z*z + c) c
 
 eachPoint width height f =
-  mapM_ (\(x, y) -> f x y (escapeTime ((-2.0 + 3.0 / width * x) :+ (-1.0 + 2.0 / height * y)))) [(x, y) | y <- [0..height], x <- [0..width]]
+  mapM_ (\(x, y) -> f x y $ escapeTime $ (-2.0 + 3.0 / width * x) :+ (-1.0 + 2.0 / height * y)) [(x, y) | y <- [0..height], x <- [0..width]]
 
-main = eachPoint width height (\x y v -> if x == 0 then putStrLn "" else putStr(if v < 255 then "-" else "#"))
+main = eachPoint width height $ \x y v -> if x == 0 then putStrLn "" else putStr $ if v < 255 then "-" else "#"
