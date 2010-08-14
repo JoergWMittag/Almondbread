72c8b6f22aa50f85d7864cdc191b092eab85c6ec
Jörg W Mittag <JoergWMittag+Almondbread@GoogleMail.Com>
2010-08-14 16:47:41 +0200

    Yet more functional version of escapeTime.

--- haskell/almondbread.hs
+++ haskell/almondbread.hs
@@ -3,11 +3,12 @@ import Data.Complex
 width = 80
 height = 20
 
-escapeTime c = escapeTime' (0.0 :+ 0.0) 0
+escapeTime c = length (take 255 (escapeSeries c))
   where
-    escapeTime' z step
-      | step > 254 || magnitude (abs z) > 4.0 = step
-      | otherwise                             = escapeTime' (z*z + c) (step + 1)
+    escapeSeries c = takeWhile notEscaping mandelSeries
+      where
+        notEscaping  = \z -> magnitude (abs z) < 4.0
+        mandelSeries = iterate (\z -> z*z + c) c
 
 eachPoint width height f =
   mapM_ (\(x, y) -> f x y (escapeTime ((-2.0 + 3.0 / width * x) :+ (-1.0 + 2.0 / height * y)))) [(x, y) | y <- [0..height], x <- [0..width]]
